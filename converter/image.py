be58cc4cd0239bb861c7b477f58406fbc33a57f1
_Nailyk_ <kykydesavoie@hotmail.com>
2019-03-29 11:17:46 +0100

    Création pixelToNum et modification numToPixel

--- converter/image.py
+++ converter/image.py
@@ -6,18 +6,24 @@ from utils import *
 
 class Image:
 
-    def __init__(self, msgEncode=None, image=None):
+    def __init__(self, objMsg=None, image=None):
         """Prend en paramètres un objet Message ou une image et produit l'inverse"""
         checkInputs(msgEncode, image)
 
-        self.msgEncode = msgEncode
+        self.msgEncode = objMsg.msg # Tableau de int
         self.algoNumber = None  # Checksum de l'algo ?
 
     def numToPixel(self, nb):
-        """Retourne un tuple RGB à partir d'un nombre
+        """Retourne une liste RGB à partir d'un nombre
             On part du principe qu'il y a 84 charactère (printable[:-16])
         """
+
         return [nb, nb, nb]
+
+    def pixelToNum(self,RGB):
+        """Retourne un nombre à partir d'un nombre RGB
+        """
+        
 
     def assemble(self, algoNumber):
         """Prend un tableau de pixel et rajoute les codes d'identifications de l'algorithme"""
