9a61d09fdd5f79c3102a049c68c37e78e77b6af1
Simon <simon.leonard@sfr.fr>
2019-04-24 21:21:02 +0200

    Ajout fonction qui renvoie le hash d'une autre fonction

--- converter/utils.py
+++ converter/utils.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
+from inspect import getsource
+from hashlib import md5
+
 
 def checkInputs(a, b):
     if a == None and b == None:
@@ -9,3 +12,11 @@ def checkInputs(a, b):
         raise ValueError("Seulement un des deux paramètres doit être renseigné")
     else:
         pass
+
+
+def funcChecksum(func):
+    """Renvoie le hash md5 d'un fonction"""
+    code = getsource(func)
+    code = "".join([line.strip() for line in code.split("\n") if line != ''])
+    hash = md5(code.encode())
+    return hash.hexdigest()
