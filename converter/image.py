2d92258ad1bf65397f82f32cc88fc76f21c51b8f
Simon <simon.leonard@sfr.fr>
2019-05-02 23:34:23 +0200

    refonte de assemble, completion __init__

--- converter/image.py
+++ converter/image.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-from utils import *
+import utils
 from PIL import *
+from random import randint
 
 
 class Image:
 
-    def __init__(self, objMsg=None, image=None, parcourir=lineaire):
-        """Prend en paramètres un objet Message ou une image et produit l'inverse"""
+    def __init__(self, objMsg=None, image=None, sizeImg=None, parcourir=utils.lineaire):
+        """Prend en paramètres un objet Message ou une image et produit l'inverse
+            A besoin d'un générateur pour connaître l'ordre de lecture
+            A besoin d'un tuple sizeImg (largeur,hauteur) > nb_caractères/3 + 2 (pixels de positions) +
+        """
         checkInputs(msgEncode, image)
+        if type(sizeImg) != tuple:
+            raise ValueError("Il manque la valeur de sizeImg")
+
+        #if sizeImg[0]*sizeImg[1] < len()
+        self.sizeImg = sizeImg
         self.parcourir = parcourir
-        self.msgEncode = objMsg.msg  # Tableau de int
-        self.algoNumber = None  # Checksum de l'algo ?
+        self.msgEncode = objMsg.getEncodedMsg()  # Tableau de int
+        self.cryptChecksum = objMsg.getCryptChecksum
 
     def numToPixel(self, nb):
         """Retourne une liste RGB à partir d'un nombre
             On part du principe qu'il y a 84 charactère (printable[:-16])
         """
-        nbR = nb//16
-        nbG = int((nb*nb)/300)
-        nbV = nb % 16
-        R = nbR
-        G = nbG
-        B = nbV
+        R = nb//16
+        G = int((nb*nb)/300)
+        V = nb % 16
         return [R, G, B]
 
     def pixelToNum(self, RGB):
@@ -35,23 +41,53 @@ class Image:
         nb = nbR*16 + nbV
         return [nb]
 
-    def assemble(self, size, RGBTab, newVal):
-        """Prend un tableau de pixel et rajoute les codes d'identifications de l'algorithme"""
-        if size in RGBTab[0][1] or size in RGBTab[0][2]:
-            return RGBTab.append(newVal)
-        else:
-            im = Image.new('RGB', size)
+    def assemble(self, RGBTab):
+        """Prend un tableau de pixel et rajoute un pixel au debut (position début checksums) et 11 à la fin (checksums)
+            Retourne un tableau de pixel (une dimension)
+            size: tuple (largeur,hauteur)
+            RGBTab: liste sous la forme [[r1, g1, b1], [r2, g2, b2], ...]
+        """
+
+        positionDebutChecksums = 1 + len(RGBTab)
+        # On convertie en liste de bit
+        positionDebutChecksums = list(bin(positionDebutChecksums)[2:])
+        # On remplie avec des 0 si besoin
+        while len(positionDebutChecksums) < 16:
+            positionDebutChecksums.insert(0, "0")
+        # On écrit les valeurs du premier pixel
+        Rposition = int("".join(positionDebutChecksums[:8]), 2)
+        Gposition = int("".join(positionDebutChecksums[8:]), 2)
+        Bposition = randint(0, 255)
+        position = [Rposition, Gposition, Bposition]
+        # Par déduction:
+        # px[0] > lire position debut params
+        # msg = px[1]...px[positionDebutParams-1]
+        # checksums = px[positionDebutParams]..px[positionDebutParams+6*2]
+
+        parcourirChecksum = utils.funcChecksum(self.parcourir)
+        checksums = list(parcourirChecksum + self.cryptChecksum)
+        # Convertion en tableau de valeurs entre 0 et 255
+        pxChecksums = [ int(checksums[x] + checksums[x+1], 16)
+                       for x in range(len(checksums))
+                       if x % 2 == 0]
+        # Regroupement par 3
+        pxChecksumsFinal = [ [ pxChecksums[x], pxChecksums[x+1], pxChecksums[x+2] ]
+                        for x in range(len(pxChecksums)-2)
+                        if x % 3 == 0]
+        # Ajout des deux pixels menquant
+        pxChecksumsFinal.append([ pxChecksums[-2], pxChecksums[-1], randint(0, 255) ])
 
 
-<< << << < HEAD
-im.append(newVal)
-== == == =
->>>>>> > parcourir
+
+        final = []
+        final.extend(position)
+        final.extend(RGBTab)
+        final.extend(pxChecksumsFinal)
 
 
-def lecture(self, imgTab):
-    """Retourne des listes de valeurs RGB"""
-    R = imgTab[0]
-    G = imgTab[1]
-    B = imgTab[2]
-    return [R, G, B]
+    def lecture(self, imgTab):
+        """Retourne des listes de valeurs RGB"""
+        R = imgTab[0]
+        G = imgTab[1]
+        B = imgTab[2]
+        return [R, G, B]
